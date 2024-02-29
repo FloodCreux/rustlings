82444d0064b170d76fa6f01e496592f735181f2d
Mike Flood <flood.mike@gmail.com>
2024-02-29 15:09:42 -0600

    Complete exercise 13: Errors

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -11,11 +11,14 @@
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input) {
+        Ok(c) => c,
+        Err(e) => return Err(e),
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -23,6 +26,8 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
